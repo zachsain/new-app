9527976dc8d1ffc67ddcd1539554f79ef1c33b01
Zachary Sain <zachsain@gmail.com>
2023-01-03 13:19:10 -0600

    initial setup

--- app/controllers/fallback_controller.rb
+++ app/controllers/fallback_controller.rb
@@ -2,6 +2,7 @@
 # Leave this here to help deploy your app later!
 class FallbackController < ActionController::Base
 
+  #test 
   def index
     # React app index page
     render file: 'public/index.html'
